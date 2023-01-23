28aa0e869a05556754a84f5b00c471cef9b536ae
bridiver <34129+bridiver@users.noreply.github.com>
2023-01-22 18:59:08 -0700

    Rename chromium personality routine to match the one in rust to eliminate "too many personalities" issue

---
+++ chromium_src/base/mac/call_with_eh_frame.cc
@@ -0,0 +1,27 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "base/mac/call_with_eh_frame.h"
+
+#include <stdint.h>
+#include <unwind.h>
+
+#include "build/build_config.h"
+#include "src/base/mac/call_with_eh_frame.cc"
+
+// Rename the chromium personality routine to match the one included with rust
+// so we can override it at link time.
+// See https://github.com/rust-lang/rust/issues/102754#issuecomment-1399669725
+#if defined(__x86_64__) || defined(__aarch64__)
+extern __attribute__((weak)) "C" _Unwind_Reason_Code rust_eh_personality(
+    int version,
+    _Unwind_Action actions,
+    uint64_t exception_class,
+    struct _Unwind_Exception* exception_object,
+    struct _Unwind_Context* context) {
+  return base::mac::CxxPersonalityRoutine(version, actions, exception_class,
+                                          exception_object, context);
+}
+#endif  // defined(__x86_64__) || defined(__aarch64__)
