4860469e4ed55536d6777901817a55c6e0048e10
Aleksey Khoroshilov <akhoroshilov@brave.com>
2023-02-02 20:20:15 +0700

    Support multiple ninja target builds at once.

--- build/commands/lib/config.js
+++ build/commands/lib/config.js
@@ -928,7 +928,7 @@ Config.prototype.update = function (options) {
     this.extraNinjaOpts.push('--offline')
   }
 
-  if (options.target) {
+  if (options.target?.length) {
     this.buildTarget = options.target
   }
 
